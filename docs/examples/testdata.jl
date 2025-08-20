5c937ab819c304d00bf1e323707f5dad11dd9e7a
Hans Würfel <git@wuerfel.io>
2025-08-20 13:44:38 +0200

    add questions to documentation

--- docs/examples/testdata.jl
+++ docs/examples/testdata.jl
@@ -12,14 +12,13 @@ nothing #hide
 #=
 !!! tip
     Hover the nodes to inspect the properties!
-
 =#
 reduced_dataset = reduce_complexity(dataset)
 inspect_collection(reduced_dataset; edge_labels=false, node_labels=:short, size=(1000,1000))
 #-
 PowerDynamicsCGMES.add_property_hover(current_figure(), reduced_dataset) #hide
 #=
-Well thats still a bit to much. Let split the dataset topologicially
+Well, that's still a bit too much. Let's split the dataset topologically
 =#
 nodes, edges = split_topologically(dataset; warn=false)
 nothing #hide
@@ -29,6 +28,10 @@ nothing #hide
 inspect_collection(nodes[1]; size=(900,900))
 #-
 PowerDynamicsCGMES.add_property_hover(current_figure(), nodes[1]) #hide
+#=
+**Discussion:**
+Power of load and powerflow result match perfectly. I guess this is just a PQ node for the powerflow.
+=#
 
 #=
 ## Bus 2: Load + Machine
@@ -36,6 +39,11 @@ PowerDynamicsCGMES.add_property_hover(current_figure(), nodes[1]) #hide
 inspect_collection(nodes[2]; size=(900,900))
 #-
 PowerDynamicsCGMES.add_property_hover(current_figure(), nodes[2]) #hide
+#=
+**Discussion:**
+- Synchronous machine has power P=Q=0, no voltage controller
+- The bus power matches the load power, so I guess once again this is a PQ node?
+=#
 
 #=
 ## Bus 3: Three Machines
@@ -43,6 +51,14 @@ PowerDynamicsCGMES.add_property_hover(current_figure(), nodes[2]) #hide
 inspect_collection(nodes[3]; size=(900,900))
 #-
 PowerDynamicsCGMES.add_property_hover(current_figure(), nodes[3]) #hide
+#=
+**Discussion:**
+- Why is the regulating control connected to a different terminal equipment than the machine?
+- The powerflow result on both machine terminals is not even close to the machine setpoint (nor power factor)
+- The voltage of the topological node does not match the reference value for the voltage regulator
+- On the third machine, the PQ setpoints are consistent with the powerflow result
+- The overall bus power (as seen by state value of connected transformer) is P=-0.47 and Q=0.019, so the SvPowerflow values seem consistent
+=#
 
 #=
 ## Powerline 1: AC Pi-Line
@@ -50,6 +66,10 @@ PowerDynamicsCGMES.add_property_hover(current_figure(), nodes[3]) #hide
 inspect_collection(edges[1]; size=(900,900))
 #-
 PowerDynamicsCGMES.add_property_hover(current_figure(), edges[1]) #hide
+#=
+**Discussion:**
+Seems fine.
+=#
 
 #=
 ## Powerline 2: Transformer
@@ -57,3 +77,8 @@ PowerDynamicsCGMES.add_property_hover(current_figure(), edges[1]) #hide
 inspect_collection(edges[2]; size=(900,900))
 #-
 PowerDynamicsCGMES.add_property_hover(current_figure(), edges[2]) #hide
+#=
+**Discussion:**
+- Both transformer ends have b/g and r/x, so this can be interpreted as a pi-line with two bases and r1+r2 / x1+x2 impedance.
+- The RatioTapChanger points at one transformer end but regulates the voltage at the other transformer end. Does it mean it acts on one end to control voltage on the other end?
+=#
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,27 +1,6 @@
 ```@meta
 CurrentModule = PowerDynamicsCGMES
 ```
+# PowerDynamicsCGMES Docs
 
-```@example wgl
-using CairoMakie
-using GraphMakie
-using Graphs
-using Markdown
-using PowerDynamicsCGMES
-
-g = smallgraph(:karate)
-fig = Figure()
-ax = Axis(fig[2,2]);
-Label(fig[1,1], "fobarbarxo\nfobarlkj\nlkjasdf")
-#Label(fig[2,1], "bar", tellwidth=false)
-p = graphplot!(g)
-hidespines!(ax)
-hidedecorations!(ax)
-fig
-```
-```@example wgl
-PowerDynamicsCGMES.html_hover_map(fig, repr.(1:34)) # hide
-```
-fobar
-balskej
-# section
+Experimental package to explor options to create PowerDynamics models from CIM/CGMES files.
