1035351e8254e3403b1a60da0d0d3bd92240324e
Hans Würfel <git@wuerfel.io>
2025-08-21 10:08:37 +0200

    parse the sections into data frames

--- src/PSSE/PSSE.jl
+++ src/PSSE/PSSE.jl
@@ -4,6 +4,27 @@ using CSV
 using DataFrames
 using OrderedCollections
 
+export parse_raw_file
+
+function parse_raw_file(file; verbose=true)
+    glob = PSSE.parse_global(file)
+    rev = glob.rev
+    raw_sections = PSSE.find_raw_sections(file)
+    parsed_sections = OrderedDict{String,DataFrame}()
+    for (name, lines) in raw_sections
+        if  isempty(Iterators.filter(!iscomment, lines))
+            verbose && printstyled("Skip empty $name section... \n", color=:blue)
+        elseif PSSE.istable(lines)
+            verbose && printstyled("Try parsing $name section... \n", color=:blue)
+            df = PSSE.parse_table_section(rev, name, lines)
+            parsed_sections[name] = df
+        else
+            printstyled("Skip section $name, no parser available.\n", color=:red)
+        end
+    end
+    parsed_sections
+end
+
 function parse_global(file)
     globpattern = r"^\s*(\d+),\s*([\d.]+),\s*(\d+),\s*(\d+),\s*(\d+),\s*([\d.]+)"
     m = nothing
@@ -73,21 +94,101 @@ function find_raw_sections(file)
         end
     end
 
+
+    # read file
+    lines = readlines(file)
+
     # Create an ordered dictionary to hold the section ranges
-    sections = OrderedDict{String,UnitRange}()
+    sections = OrderedDict{String,Vector{String}}()
     current_section = nothing
     for s in sectionbreaks
         if isnothing(current_section)
             current_section = (; name = s.begin_of, start = s.i+1)
         elseif s.end_of == current_section.name
             range = current_section.start:(s.i-1)
-            sections[current_section.name] = range
+            sections[current_section.name] = lines[range]
             current_section = (; name = s.begin_of, start = s.i+1)
         else
             error("Unexpected section break: $s while in section $(current_section)")
         end
     end
     sections
+end
+
+KNOWN_HEADERS = OrderedDict(
+    30 => OrderedDict(
+        "BUS" => ["i", "name", "basekv", "ide", "gl", "bl", "area", "zone", "vm", "va", "owner"],
+        "BRANCH" => ["i", "j", "ckt", "r", "x", "b", "rate_a", "rate_b", "rate_c", "gi", "bi", "gj", "bj", "st", "len", "o1", "f1", "o2", "f2", "o3", "f3", "o4", "f4"],
+        "LOAD" => ["i", "id", "status", "area", "zone", "pl", "ql", "ip", "iq", "yp", "yq", "owner", "scale", "intrpt"],
+        "GENERATOR" => ["i", "id", "pg", "qg", "qt", "qb", "vs", "ireg", "mbase", "zr", "zx", "rt", "xt", "gtap", "stat", "rmpct", "pt", "pb", "o1", "f1", "o2", "f2", "o3", "f3", "o4", "f4", "wmod", "wpf"],
+        "FIXED SHUNT" => ["i", "id", "status", "gl", "bl"],
+        "AREA" => ["i", "isw", "pdes", "ptol", "arname"],
+        "SWITCHED SHUNT" => ["i", "modsw", "vswhi", "vswlo", "swrem", "rmpct", "rmidnt", "binit", "n1", "b1", "n2", "b2", "n3", "b3", "n4", "b4", "n5", "b5", "n6", "b6", "n7", "b7", "n8", "b8"],
+        "ZONE" => ["i", "zoname"],
+        "OWNER" => ["i", "owname"]
+    ),
+    33 => OrderedDict(
+        "BUS" => ["i", "name", "basekv", "ide", "area", "zone", "owner", "vm", "va", "nvhi", "nvlo", "evhi", "evlo"],
+        "BRANCH" => ["i", "j", "ckt", "r", "x", "b", "rate_a", "rate_b", "rate_c", "gi", "bi", "gj", "bj", "st", "met", "len", "o1", "f1", "o2", "f2", "o3", "f3", "o4", "f4"],
+        "LOAD" => ["i", "id", "status", "area", "zone", "pl", "ql", "ip", "iq", "yp", "yq", "owner", "scale"],  # Note: some files may have 14th column "intrpt" (interruptible load flag)
+        "GENERATOR" => ["i", "id", "pg", "qg", "qt", "qb", "vs", "ireg", "mbase", "zr", "zx", "rt", "xt", "gtap", "stat", "rmpct", "pt", "pb", "o1", "f1", "o2", "f2", "o3", "f3", "o4", "f4", "wmod", "wpf"],
+        "FIXED SHUNT" => ["i", "id", "status", "gl", "bl"],
+        "AREA" => ["i", "isw", "pdes", "ptol", "arname"],
+        "SWITCHED SHUNT" => ["i", "modsw", "adjm", "stat", "vswhi", "vswlo", "swrem", "rmpct", "rmidnt", "binit", "n1", "b1", "n2", "b2", "n3", "b3", "n4", "b4", "n5", "b5", "n6", "b6", "n7", "b7", "n8", "b8"],
+        "ZONE" => ["i", "zoname"],
+        "OWNER" => ["i", "owname"]
+    )
+)
+
+iscomment(l) = startswith(l, r"\s*@!")
+
+function istable(lines)
+    allequal(Iterators.filter(!iscomment, lines)) do l
+        count(",", l)
+    end
+end
+
+function parse_table_section(rev, name, lines)
+    # detuct the header
+    _header = determine_header(rev, name, lines)
+    header = isnothing(_header) ? 0 : _header
+
+    io = IOBuffer()
+    for l in lines
+        println(io, l)
+    end
+    seekstart(io)
+
+    CSV.read(io, DataFrame; header, normalizenames=true, quotechar=''', comment="@!", silencewarnings=true)
+end
+
+function determine_header(rev, name, lines)
+    header = parse_header_commment(name, lines)
+    !isnothing(header) && return header
+
+    if haskey(KNOWN_HEADERS, rev) && haskey(KNOWN_HEADERS[rev], name)
+        return KNOWN_HEADERS[rev][name]
+    end
+    @warn "Don't know the header, using default!"
+
+end
+
+function parse_header_commment(name, lines)
+    commented = findall(iscomment, lines)
+    if isempty(commented)
+        return nothing
+    else
+        length(commented) == 1 || throw(ArgumentError("Expected exactly one comment line in section $name, found $(length(commented))"))
+        headerline = lines[only(commented)]
+        header = split(headerline, ",")
+        header_normalized = map(header) do s
+            s = replace(s, r"^\s*@!" => "") # remove comments
+            s = replace(s, "'" => "") # remove single quotes
+            s = replace(s, r"^\s*|\s*$" => "") # remove leading and trailing whitespace
+            lowercase(s)
+        end
+        return header_normalized
+    end
 end
 
 end
--- test/PSSE/parsing_test.jl
+++ test/PSSE/parsing_test.jl
@@ -5,6 +5,7 @@ using PowerDynamicsParsers.PSSE: PSSESection
 f1 = joinpath(pkgdir(PowerDynamicsParsers), "test", "PSSE", "data", "IEEE 39 bus.RAW")
 f2 = joinpath(pkgdir(PowerDynamicsParsers), "test", "PSSE", "data", "Hawaii40_20231026.RAW")
 f3 = joinpath(pkgdir(PowerDynamicsParsers), "test", "PSSE", "data", "Texas2k_series24_case1_2016summerPeak.RAW")
+f4 = joinpath(pkgdir(PowerDynamicsParsers), "test", "PSSE", "data", "RTS-GMLC.RAW")
 
 PSSE.parse_raw(file)
 
@@ -19,7 +20,22 @@ file = f1
 file = f2
 file = f3
 
-sb1 = PSSE.find_raw_sections(f1)
 sb2 = PSSE.find_raw_sections(f2)
 sb3 = PSSE.find_raw_sections(f3)
 
+sections = PSSE.find_raw_sections(f1)
+
+name = "BUS"
+lines = sb3["BUS"]
+
+
+file = f1
+
+
+parse_raw_file(f1)
+parse_raw_file(f2)
+parse_raw_file(f3)
+parse_raw_file(f4)
+
+PSSE.KNOWN_HEADERS[30]["SWITCHED SHUNT"]
+PSSE.KNOWN_HEADERS[33]["SWITCHED SHUNT"]
