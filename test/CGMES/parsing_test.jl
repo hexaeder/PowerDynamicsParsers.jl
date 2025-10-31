2b78c423b7a64b9c60d7997d62cbabfda21a4cf8
Hans Würfel <git@wuerfel.io>
2025-10-31 10:59:43 +0100

    fix testset spelling

--- test/CGMES/parsing_test.jl
+++ test/CGMES/parsing_test.jl
@@ -3,7 +3,7 @@ using PowerDynamicsParsers.CGMES
 
 DATA = joinpath(pkgdir(PowerDynamicsParsers), "test", "CGMES", "data")
 
-@testest "parse testdata" begin
+@testset "parse testdata" begin
     for (path, dirs, files) in walkdir(DATA)
         if any(endswith(".xml"), files)
             println("Parsing dataset $(replace(path, DATA * "/" => ""))")
